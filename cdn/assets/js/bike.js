6d8ba707a19b10e0eecf3d31791856261103ecc2
Leonardo <leofiore@libero.it>
2012-08-22 16:33:44 +0200

    invertito ordine dei mediaplugin (flash, silverlight)

--- cdn/assets/js/bike.js
+++ cdn/assets/js/bike.js
@@ -30,7 +30,7 @@ $(window).ready(function(evt) {
 
     me = new MediaElement('audiosrc', {
         enablePluginDebug: true,
-        plugins: ['silverlight', 'flash'],
+        plugins: ['flash', 'silverlight'],
         type: 'audio/mp3',
         pluginPath: 'http://cdn.radiocicletta.it/js/me/',
         flashName: 'flashmediaelement.swf',
