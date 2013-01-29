ee9dc4f9a07283875830f2143628089b974fb130
Leonardo <leofiore@libero.it>
2013-01-29 12:14:12 +0100

    Miglioramenti su profilo utente.
        Nel rispetto della privacy ora si puo' usare un nickname
        aggiuntivo come unico identificativo visualizzato.
    
        +paranoia, +gioia

--- radiocicletta-server/profiles/views.py
+++ radiocicletta-server/profiles/views.py
@@ -1,16 +1,25 @@
 from django.contrib.auth.models import User
+from profiles.models import UserProfile
 from blog.models import Post
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
+
 
 def show_profile(request, nickname):
     profile = None
     if nickname:
-        user = User.objects.filter(username = nickname)
-        if user :
-            profile = user[0].profile
-            posts = Post.objects.filter(author = user[0]).order_by('-published_on')
+        profile = UserProfile.objects.filter(nickname=nickname)
+        if profile:
+            user = profile[0].user
+            profile = profile[0]
+            posts = Post.objects.filter(author=user).order_by('-published_on')
+        else:
+            user = User.objects.filter(username=nickname)
+            if user:
+                profile = user[0].profile
+                posts = Post.objects.filter(author=user[0]).order_by('-published_on')
     return render(request, 'profiles/show.html', {'profile': profile, 'posts': posts})
 
+
 def all_profiles(request):
-    profiles = UserProfile.object.all()
+    profiles = UserProfile.objects.all()
     return render(request, 'profiles/list.html', {'profiles': profiles})
